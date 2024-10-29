5b6f3845a6c5da589325ed31d9e36e25e2f24db8
Ronit Ramnarayan <ronitvjramnarayan@gmail.com>
2024-10-29 15:56:40 -0400

    Created Customer Controller and Model

---
+++ CustomersAJAX/Models/Customer.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace CustomersAJAX.Models
+{
+    public class Customer
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public int Age { get; set; }
+
+        public Customer(int id, string name, int age)
+        {
+            ID = id;
+            Name = name;
+            Age = age;
+        }
+    }
+
+}
